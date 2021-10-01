1a3d933bbef910bae2bce88a3c6b8f798bb64376
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-01 17:17:44 -0300

    Committing "MultiplosDe3"

---
+++ AprendendoCSharp/MultiplosDe3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplosDe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Múltiplos de 3 entre 1 e 100:");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
