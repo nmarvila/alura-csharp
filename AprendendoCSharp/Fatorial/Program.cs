49957d53e8facd96e0f5db44752cb845675faad4
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-01 17:25:33 -0300

    Committing "Fatorial"

---
+++ AprendendoCSharp/Fatorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Primeira solução
+            Console.WriteLine("Fatoriais de 1 a 10:");
+            int fatorial;
+
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial = 1;
+                for (int count = 1; count <= n; count++)
+                {
+                    fatorial *= count;
+                }
+                Console.WriteLine(fatorial);
+            }
+
+            // Outra forma
+            Console.WriteLine();
+
+            fatorial = 1;
+            for (int i = 1; i < 11; i++)
+            {
+                fatorial *= i;
+                Console.WriteLine(fatorial);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
