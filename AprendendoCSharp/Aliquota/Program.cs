b53ec017ebf0621e39db4f452a6b0a7654a58cea
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-01 12:18:11 -0300

    Committing "Aliquota"

---
+++ AprendendoCSharp/Aliquota/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliquota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 3300.0;
+
+            if (salario >= 1900.0 && salario <= 2800.0)
+            {
+                Console.WriteLine("Alíquota: 7.5%");
+                Console.WriteLine("Pode deduzir até R$ 142,00");
+            }
+            if (salario >= 2800.01 && salario <= 3751.0)
+            {
+                Console.WriteLine("Alíquota: 15%");
+                Console.WriteLine("Pode deduzir até R$ 350,00");
+            }
+            if (salario >= 3751.01 && salario <= 4664.0)
+            {
+                Console.WriteLine("Alíquota: 22.5%");
+                Console.WriteLine("Pode deduzir até R$ 636,00");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
