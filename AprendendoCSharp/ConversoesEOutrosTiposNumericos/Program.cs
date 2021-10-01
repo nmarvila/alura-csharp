788490ad7643313b328d5c97800fc054f746892b
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-01 10:56:26 -0300

    Committing "ConversoesEOutrosTiposNumericos"

---
+++ AprendendoCSharp/ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pontoFlutuante = 3.14f;
+
+            double salario = 1270.50;
+            int valor = (int) salario;
+            Console.WriteLine(valor);
+
+            double valor1 = 0.2;
+            double valor2 = 0.1;
+            double total = valor1 + valor2;
+
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+        }
+    }
+}
