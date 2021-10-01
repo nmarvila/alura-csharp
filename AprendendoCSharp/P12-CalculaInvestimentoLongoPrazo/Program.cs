83842b78f13a69bf19f865c6a03b8eaf204cb7c4
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-01 16:55:52 -0300

    Committing "P12-CalculaInvestimentoLongoPrazo"

---
+++ AprendendoCSharp/P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 12");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+
+                // contadorMes = 5;
+                fatorRendimento += 0.0010;
+            }
+
+            // Console.WriteLine("Tempo de investimento foi de " + contadorMes);
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
