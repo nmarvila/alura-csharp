e3d71660291141218517c7f1d428b49da705997f
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-05 12:33:24 -0300

    Committing "C# - Parte 4" first lesson

---
+++ Parte 4/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ContaCorrente conta = new ContaCorrente(7480, 874150);
+
+            // Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+
+            }
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+    }
+}
