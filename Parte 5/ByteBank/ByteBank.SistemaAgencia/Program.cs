6a8b96d57bc79ddcfa072c3c8d912c0c19bbd54a
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-06 16:07:13 -0300

    Committing "C# - Parte 5" second lesson

--- Parte 5/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Parte 5/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,6 +13,12 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            FuncionarioAutenticavel carlos = null;
+
+            carlos.GetBonificacao();
+
+            carlos.Autenticar("fasfasdfasdf");
 
             Console.WriteLine(conta.Numero);
 
