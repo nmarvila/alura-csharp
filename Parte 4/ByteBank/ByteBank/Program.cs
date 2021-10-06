fdd1be9280b275809110c1dd1a3aabc8847306c2
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-06 10:52:48 -0300

    Committing "C# - Parte 4" sixth lesson

--- Parte 4/ByteBank/ByteBank/Program.cs
+++ Parte 4/ByteBank/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,26 @@ namespace ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta1 = new ContaCorrente(4564, 789684);
                 ContaCorrente conta2 = new ContaCorrente(7891, 456794);
 
                 // conta1.Transferir(10000, conta2);
                 conta1.Sacar(10000);
-
             }
             catch (OperacaoFinanceiraException e)
             {
@@ -27,19 +42,44 @@ namespace ByteBank
                 // Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
 
             }
-
-            Console.WriteLine("Execução finalizada. Tecle enter para sair");
-            Console.ReadLine();
         }
 
-        // Teste com a cadeia de chamada:
-        // Metodo -> TestaDivisao -> Dividir
-        static void Metodo()
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+    
+
+
+            // ---------------------------------------------
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contasl.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+
+        private static void Metodo()
         {
             TestaDivisao(0);
         }
 
-        static void TestaDivisao(int divisor)
+        private static void TestaDivisao(int divisor)
         {
             try
             {
@@ -59,7 +99,7 @@ namespace ByteBank
             Console.WriteLine("Estou executando normalmente!");
         }
 
-        public static int Dividir(int numero, int divisor)
+        private static int Dividir(int numero, int divisor)
         {
             try
             {
