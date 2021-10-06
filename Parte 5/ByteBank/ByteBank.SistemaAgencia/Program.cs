3acb254d98b109164de8df619e2a23ad1f742f4f
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-06 17:59:30 -0300

    Committing "C# - Parte 5" fifth lesson

--- Parte 5/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Parte 5/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,12 +13,14 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
+            DateTime dataFimPagamento = new DateTime(2021, 10, 12);
+            DateTime dataCorrente = DateTime.Now;
 
-            FuncionarioAutenticavel carlos = null;
-            carlos.Autenticar("fasfasdfasdf");
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
 
-            Console.WriteLine(conta.Numero);
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
