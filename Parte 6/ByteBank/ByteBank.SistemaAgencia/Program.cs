85668261c2db75cdd952b652c31f4cfa9e4c3364
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-07 15:35:24 -0300

    Committing "C# - Parte 6" first lesson

---
+++ Parte 6/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pagina?argumentos
+            // 012345678
+
+            string url = "paginas?argumentos";
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(8);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+}
