ad698ab32a435ac7024e449e781a214c57b14c8f
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-23 21:40:11 -0300

    Committing "C# Collections - Parte 1" second lesson

---
+++ CSharp Collections/Parte 1/CSharpCollections/Aula24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Collections", "Marcelo Oliveira");
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            Imprimir(csharpColecoes.Aulas);
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
--- CSharp Collections/Parte 1/CSharpCollections/CSharpCollections/Program.cs
+++ CSharp Collections/Parte 1/CSharpCollections/CSharpCollections/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpCollections
+namespace Aula1
 {
     internal class Program
     {
