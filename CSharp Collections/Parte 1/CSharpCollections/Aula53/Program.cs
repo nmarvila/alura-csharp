a87669992242a559f7e1da05518ca88239fe275a
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-26 21:11:28 -0300

    Committing "C# Collections - Parte 1" fifth lesson

---
+++ CSharp Collections/Parte 1/CSharpCollections/Aula53/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula53
+{
+    internal class Program
+    {
+        static Queue<string> pedagio = new Queue<string>();
+
+        static void Main(string[] args)
+        {
+            Enfileirar("van");
+            Enfileirar("kombi");
+            Enfileirar("guincho");
+            Enfileirar("pickup");
+
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();
+        }
+
+        private static void Desenfileirar()
+        {
+            if (pedagio.Any())
+            {
+                if (pedagio.Peek() == "guincho")
+                {
+                    Console.WriteLine("guincho está fazendo o pagamento.");
+                }
+
+                string veiculo = pedagio.Dequeue();
+                Console.WriteLine($"Saiu da fila: {veiculo}");
+                ImprimirFila();
+            }
+        }
+
+        private static void Enfileirar(string veiculo)
+        {
+            Console.WriteLine($"Entrou na fila: {veiculo}");
+            pedagio.Enqueue(veiculo);
+            ImprimirFila();
+        }
+
+        private static void ImprimirFila()
+        {
+            Console.WriteLine("FILA:");
+            foreach (var v in pedagio)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
