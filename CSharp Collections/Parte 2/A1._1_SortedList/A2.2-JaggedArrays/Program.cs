59413b9cb5d8f7550693414e895be151ed6bd0da
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-29 21:18:14 -0300

    Committing "C# Collections - Parte 2" second lesson

---
+++ CSharp Collections/Parte 2/A1._1_SortedList/A2.2-JaggedArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2._2_JaggedArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] familias = new string[3][];
+            //{
+            //    { "Fred", "Wilma", "Pedrita" },
+            //    { "Homer", "Marge", "Lisa", "Bart", "Maggie" },
+            //    { "Florinda", "Kiko" }
+            //};
+
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familias[2] = new string[] { "Florinda", "Kiko" };
+
+            foreach (var familia in familias)
+            {
+                Console.WriteLine(string.Join(",", familia));
+            }
+        }
+    }
+}
