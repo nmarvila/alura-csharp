27994e60f3996160829b0e4a6127f5a330b0b4af
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-30 21:23:22 -0300

    Committing "C# Refatoração - Parte 1" first lesson

---
+++ CSharp Refatoração/CSharpRefatoracao/refatoracao/Aula01/R02.InlineMethod/antes/Motoboy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refatoracao.Aula01.R02.InlineMethod.antes
+{
+    internal class Motoboy
+    {
+        private int qtdeEntregasNoturnas;
+
+        int GetAvaliacao()
+        {
+            return (TemMaisDeCincoEntregasNoturnas()) ? 2 : 1;
+        }
+
+        bool TemMaisDeCincoEntregasNoturnas()
+        {
+            return qtdeEntregasNoturnas > 5;
+        }
+    }
+}
---
+++ CSharp Refatoração/CSharpRefatoracao/refatoracao/Aula01/R02.InlineMethod/depois/Motoboy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refatoracao.Aula01.R02.InlineMethod.depois
+{
+    internal class Motoboy
+    {
+        private int qtdeEntregasNoturnas;
+
+        int GetAvaliacao()
+        {
+            return (qtdeEntregasNoturnas > 5) ? 2 : 1;
+        }
+    }
+}
