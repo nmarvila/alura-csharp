8d2af24ba1310f3ef847ad34f1739ca074742a33
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-30 21:47:07 -0300

    Committing "C# Refatoração - Parte 1" second lesson

---
+++ CSharp Refatoração/CSharpRefatoracao/refatoracao/Aula02/R04.InlineTemp/antes/CalculadoraDePrecos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refatoracao.Aula02.R04.InlineTemp.antes
+{
+    internal class CalculadoraDePrecos
+    {
+        bool TemDesconto(Pedido pedido)
+        {
+            decimal valorProdutos = pedido.ValorProdutos();
+            return (valorProdutos > 1000);
+        }
+    }
+
+    class Pedido
+    {
+        private readonly DateTime clienteDesde;
+        private readonly decimal valorProdutos;
+
+        public Pedido(DateTime clienteDesde, decimal valorProdutos)
+        {
+            this.clienteDesde = clienteDesde;
+            this.valorProdutos = valorProdutos;
+        }
+
+        public decimal ValorProdutos()
+        {
+            return valorProdutos;
+        }
+
+        public bool TemDesconto()
+        {
+            //aqui NÃO É um bom exemlo para inline method!
+            bool clienteHaMaisDe5Anos = (DateTime.Today.Subtract(clienteDesde).TotalDays / 365) >= 5;
+            bool compraEspecial = valorProdutos > 1000;
+            return clienteHaMaisDe5Anos && compraEspecial;
+        }
+    }
+}
