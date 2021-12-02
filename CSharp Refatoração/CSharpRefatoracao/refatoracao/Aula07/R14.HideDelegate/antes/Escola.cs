e482d5ce188fc0f0fd2521fa1701409b42076e88
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-12-01 21:32:54 -0300

    Committing "C# Refatoração - Parte 1" seventh lesson

---
+++ CSharp Refatoração/CSharpRefatoracao/refatoracao/Aula07/R14.HideDelegate/antes/Escola.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refatoracao.Aula07.R14.HideDelegate.antes
+{
+    internal class Escola
+    {
+        public string Nome { get; private set; }
+        public Funcionario Diretor { get; private set; }
+    }
+
+    class Departamento
+    {
+        public Escola Escola { get; private set; }
+    }
+
+    class Funcionario
+    {
+        public Departamento Departamento { get; private set; }
+    }
+
+    class Teste
+    {
+        public Teste()
+        {
+            var maria = new Funcionario();
+            var diretorDaMaria = maria.Departamento.Escola.Diretor;
+        }
+    }
+}
---
+++ CSharp Refatoração/CSharpRefatoracao/refatoracao/Aula07/R15.RemoveMiddleMan/depois/Escola.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refatoracao.Aula07.R15.RemoveMiddleMan.depois
+{
+    internal class Escola
+    {
+        public string Nome { get; private set; }
+        public Funcionario Diretor { get; private set; }
+    }
+
+    class Departamento
+    {
+        public Escola Escola { get; private set; }
+    }
+
+    class Funcionario
+    {
+        private Departamento Departamento { get; }
+        public Funcionario Diretor
+        {
+            get
+            {
+                return Departamento.Escola.Diretor;
+            }
+        }
+
+        private decimal salario;
+        public decimal Salario
+        {
+            get { return salario; }
+            set { salario = value; }
+        }
+    }
+
+    class Teste
+    {
+        public Teste()
+        {
+            var maria = new Funcionario();
+            var diretorDaMaria = maria.Diretor;
+        }
+    }
+}
