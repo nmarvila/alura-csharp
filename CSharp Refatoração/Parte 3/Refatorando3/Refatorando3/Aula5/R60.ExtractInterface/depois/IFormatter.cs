47bcc6d86baf2226a53edf2ee62e7395edf2cff2
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-12-10 21:59:07 -0300

    Committing "C# Refatoração - Parte 3" fifth and sixth lessons

---
+++ CSharp Refatoração/Parte 3/Refatorando3/Refatorando3/Aula5/R60.ExtractInterface/depois/IFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace Refatorando3.Aula5.R60.ExtractInterface.depois
+{
+    public interface IFormatter
+    {
+        bool CanBeFormatted(string value);
+        string Format(string value);
+        bool IsFormatted(string value);
+        string Unformat(string value);
+    }
+}
---
+++ CSharp Refatoração/Parte 3/Refatorando3/Refatorando3/Aula6/R64.ReplaceDelegationWithInheritance/depois/Imovel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refatorando3.Aula6.R64.ReplaceDelegationWithInheritance.depois
+{
+    class Programa
+    {
+        void Teste()
+        {
+            var imovel = 
+                new Apartamento("Rua dos Bobos, No. 0", 100000, 200);
+        }
+    }
+
+    abstract class Imovel
+    {
+        private readonly String endereco;
+        private decimal valorImovel;
+
+        public string Endereco => endereco;
+        public decimal ValorImovel { get => valorImovel; set => valorImovel = value; }
+
+        public Imovel(string endereco, decimal valorImovel)
+        {
+            this.endereco = endereco;
+            this.valorImovel = valorImovel;
+        }
+    }
+
+    class Apartamento : Imovel
+    {
+        private decimal valorCondominio;
+
+        public Apartamento(string endereco, decimal valorImovel, decimal valorCondominio)
+            : base(endereco, valorImovel)
+        {
+            this.valorCondominio = valorCondominio;
+        }
+    }
+}
